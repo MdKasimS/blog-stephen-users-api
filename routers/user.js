498192392924866d448ea865707c4edeca61d7a8
KasimSache <45384577+MdKasimS@users.noreply.github.com>
2024-11-14 20:23:06 +0530

    /api/users route is removed and kept only '/'

--- routers/user.js
+++ routers/user.js
@@ -1,31 +1,32 @@
 const express = require("express");
+const { handleUserSignup } = require("../controllers/user");
 
 const router = express.Router();
 
 //Hybrid Server for getting all users
-router.get("/api/users", async (req, res) => {
+router.get("/", async (req, res) => {
   // res.setHeader("X-MyName","Kasim Sache")
   const allUsers = await User.find({});
   res.json(allUsers);
 });
 
-router.get("/users", async (req, res) => {
-  const allUsers = await User.find({});
-  const html = `<ul>
-    ${allUsers
-      .map((user) => `<li>${user.first_name} - ${user.email}</li>`)
-      .join("")}
-    </ul>`;
+// router.get("/users", async (req, res) => {
+//   const allUsers = await User.find({});
+//   const html = `<ul>
+//     ${allUsers
+//       .map((user) => `<li>${user.first_name} - ${user.email}</li>`)
+//       .join("")}
+//     </ul>`;
 
-  res.send(html);
-});
+//   res.send(html);
+// });
 
 //Handling user of sepecified id
 //------------------------------------------------------
 
 //Combined all types of requests for similar route - /api/users/:id
 router
-  .route("/api/users/:id")
+  .route("/:id")
   .get(async (req, res) => {
     // const id = Number(req.params.id);
 
@@ -57,44 +58,46 @@ router
   });
 
 //Route for creating user in storage
-router.post("/api/users/", async (req, res) => {
-  const body = req.body;
-  console.log(`I was called`);
+// router.post("/", async (req, res) => {
+//   const body = req.body;
+//   console.log(`I was called`);
 
-  if (
-    !body ||
-    !body.first_name ||
-    !body.last_name ||
-    !body.gender ||
-    !body.email ||
-    !body.job_type
-  ) {
-    //!body.password
-    return res.status(400).json({ msg: "All fields are required..." });
-  }
+//   if (
+//     !body ||
+//     !body.first_name ||
+//     !body.last_name ||
+//     !body.gender ||
+//     !body.email ||
+//     !body.job_type
+//   ) {
+//     //!body.password
+//     return res.status(400).json({ msg: "All fields are required..." });
+//   }
 
-  let result = await User.create({
-    first_name: body.first_name,
-    last_name: body.last_name,
-    gender: body.gender,
-    email: body.email,
-    job_type: body.job_type,
-    password: body.password,
-  });
+//   let result = await User.create({
+//     first_name: body.first_name,
+//     last_name: body.last_name,
+//     gender: body.gender,
+//     email: body.email,
+//     job_type: body.job_type,
+//     password: body.password,
+//   });
 
-  console.log(User.collection);
+//   console.log(User.collection);
 
-  console.log(result);
-  return res.status(201).json({ msg: "success" });
+//   console.log(result);
+//   return res.status(201).json({ msg: "success" });
 
-  // Now shifting fromhard coding to database usage
-  // users.push({...body, id: users.length +1});
-  // fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err, data)=>{
-  //     return res.json({status: "success", id: `${users.length}`});
-  // });
+//   // Now shifting fromhard coding to database usage
+//   // users.push({...body, id: users.length +1});
+//   // fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err, data)=>{
+//   //     return res.json({status: "success", id: `${users.length}`});
+//   // });
 
-  // console.log(users);
-});
+//   // console.log(users);
+// });
+
+app.post('/', handleUserSignup)
 
 // app.patch('/api/users/:id',(req, res)=>{
 
